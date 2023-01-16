274bc62918e2e179b297c94245515fe58152d000
John <johnwilday@yahoo.com>
2023-01-16 01:35:18 -0600

    Added if, for loop and math.floor

--- script.js
+++ script.js
@@ -6,27 +6,44 @@ var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 function generatePassword() {
-  console.log("hello from generate password")
+  
+  var passwordLength = prompt("How many characters in password, between 8 and 128?")
   console.log("password length", passwordLength)
   var uppercaseCharacters = confirm("Want uppercase? Press OK for Yes")
   console.log(uppercaseCharacters)
   var lowercaseCharacters = confirm("Want lowercase? Press OK for Yes")
   var numbers = confirm("Want numbers? Press OK for Yes")
   var specialCharacters = confirm("Want special characters? Press OK for Yes")
-  var passwordLength = prompt("How many characters in password, between 8 and 128?")
+  
 var allYesResponses = "";
 
 if (uppercaseCharacters === true) {
 allYesResponses = allYesResponses.concat(upperChars)
-  
+}
 
 if (lowercaseCharacters === true){
   allYesResponses = allYesResponses.concat(lowerChars)
   
 }
-console.log(allYesResponses)
+
+if(numbers === true){
+  allYesResponses = allYesResponses + numberChars
 }
 
+if(specialCharacters === true){
+  allYesResponses = allYesResponses + specialChars
+}
+console.log(allYesResponses)
+var password = ""
+for (let index = 0; index < passwordLength; index++) {
+  
+  var randomCharacter = allYesResponses[Math.floor(Math.random() * allYesResponses.length)]
+  console.log(randomCharacter)
+  password = password + randomCharacter
+  return password
+}
+
+}
 // Assignment Code
 
 
