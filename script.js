ffbe8f9e99ed3bcfa5000ffc193cbf791006adf8
John <johnwilday@yahoo.com>
2023-01-17 02:01:42 -0600

    Completed the logic and confirmed working

--- script.js
+++ script.js
@@ -9,20 +9,32 @@ function generatePassword() {
   
   var passwordLength = prompt("How many characters in password, between 8 and 128?")
   console.log("password length", passwordLength)
+  
+ // Validate password length
+    if (passwordLength >= 8 && passwordLength <= 128){
+
+    }
+    else {
+    alert("You must enter a number between 8 and 128. Start again.") 
+    return
+  }
+ 
+  // Confirm which characters to use
   var uppercaseCharacters = confirm("Want uppercase? Press OK for Yes")
-  console.log(uppercaseCharacters)
   var lowercaseCharacters = confirm("Want lowercase? Press OK for Yes")
   var numbers = confirm("Want numbers? Press OK for Yes")
   var specialCharacters = confirm("Want special characters? Press OK for Yes")
   
 var allYesResponses = "";
 
+
+// Confirm what to include in password
 if (uppercaseCharacters === true) {
-allYesResponses = allYesResponses.concat(upperChars)
+allYesResponses = allYesResponses + upperChars
 }
 
 if (lowercaseCharacters === true){
-  allYesResponses = allYesResponses.concat(lowerChars)
+  allYesResponses = allYesResponses + lowerChars
   
 }
 
@@ -34,15 +46,15 @@ if(specialCharacters === true){
   allYesResponses = allYesResponses + specialChars
 }
 console.log(allYesResponses)
-var password = ""
+var password = "";
 for (let index = 0; index < passwordLength; index++) {
-  
+  // Pick Random Character
   var randomCharacter = allYesResponses[Math.floor(Math.random() * allYesResponses.length)]
   console.log(randomCharacter)
   password = password + randomCharacter
-  return password
+  
 }
-
+return password
 }
 // Assignment Code
 
@@ -50,9 +62,16 @@ for (let index = 0; index < passwordLength; index++) {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var passwordText = document.querySelector("#password");
+  console.log(password)
+if (password == undefined) {
+ passwordText.value = "Try Again."
+ }
+ else {
+  passwordText.value = password
+ }
+  
 
 }
 
